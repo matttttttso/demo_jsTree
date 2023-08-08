82d677830f9813f8a9dc8605c94590e97f026335
matttttttso <u2.pinetail@gmail.com>
2023-08-08 17:18:48 +0900

    operation check

--- demo_jsTree_in_SpringBoot/src/main/java/com/example/demo/jstree/app/controller/MainController.java
+++ demo_jsTree_in_SpringBoot/src/main/java/com/example/demo/jstree/app/controller/MainController.java
@@ -9,12 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.example.demo.jstree.domain.model.Area;
-import com.example.demo.jstree.domain.model.Direction;
-import com.example.demo.jstree.domain.model.Prefecture;
-import com.example.demo.jstree.domain.repository.AreaRowMapper;
-import com.example.demo.jstree.domain.repository.DirectionRowMapper;
-import com.example.demo.jstree.domain.repository.PrefectureRowMapper;
+import com.example.demo.jstree.domain.model.PrefTreeNode;
+import com.example.demo.jstree.domain.repository.PrefTreeNodeRowMapper;
 
 /**
  * Controllerクラス.
@@ -22,14 +18,10 @@ import com.example.demo.jstree.domain.repository.PrefectureRowMapper;
 @Controller
 public class MainController {
 	private final JdbcTemplate jdbcTemplate;
-	private final PrefectureRowMapper prefectureRowMapper;
-	private final AreaRowMapper areaRowMapper;
-	private final DirectionRowMapper directionRowMapper;
-	public MainController(JdbcTemplate jdbcTemplate, PrefectureRowMapper prefectureRowMapper, AreaRowMapper areaRowMapper, DirectionRowMapper directionRowMapper) {
+	private final PrefTreeNodeRowMapper prefTreeNodeRowMapper;
+	public MainController(JdbcTemplate jdbcTemplate, PrefTreeNodeRowMapper prefTreeNodeRowMapper) {
 		this.jdbcTemplate = jdbcTemplate;
-		this.prefectureRowMapper = prefectureRowMapper;
-		this.areaRowMapper = areaRowMapper;
-		this.directionRowMapper = directionRowMapper;
+		this.prefTreeNodeRowMapper = prefTreeNodeRowMapper;
 	}
 
 
@@ -41,27 +33,27 @@ public class MainController {
 	 */
 	@GetMapping()
 	String moveSearchView(Model model) {
-		String directionSQL = "SELECT id,name FROM directions ORDER BY id";
-		List<Direction> directionList = jdbcTemplate.query(directionSQL, directionRowMapper);
+		String directionSQL = "SELECT id,name,parent_id FROM directions ORDER BY id";
+		List<PrefTreeNode> directionList = jdbcTemplate.query(directionSQL, prefTreeNodeRowMapper);
 				
-		String areaSQL = "SELECT id,name,direction FROM areas ORDER BY id";
-		List<Area> areaList = jdbcTemplate.query(areaSQL, areaRowMapper);
+		String areaSQL = "SELECT id,name,parent_id FROM areas ORDER BY id";
+		List<PrefTreeNode> areaList = jdbcTemplate.query(areaSQL, prefTreeNodeRowMapper);
 		
-		String prefectureSQL = "SELECT id,name,area FROM prefectures ORDER BY id";
-		List<Prefecture> prefectureList = jdbcTemplate.query(prefectureSQL, prefectureRowMapper);
+		String prefectureSQL = "SELECT id,name,parent_id FROM prefectures ORDER BY id";
+		List<PrefTreeNode> prefectureList = jdbcTemplate.query(prefectureSQL, prefTreeNodeRowMapper);
 		
 		areaList.stream().forEach(
-				ar -> ar.setPrefectures(
+				ar -> ar.setChildNodes(
 					prefectureList.stream()
-							.filter(pr -> Objects.equals(pr.getArea(), ar.getId()))
+							.filter(pr -> Objects.equals(pr.getParentId(), ar.getId()))
 							.collect(Collectors.toList())
 				)
 		);
 		
 		directionList.stream().forEach(
-				di -> di.setAreas(
+				di -> di.setChildNodes(
 					areaList.stream()
-							.filter(ar -> Objects.equals(ar.getDirection(), di.getId()))
+							.filter(ar -> Objects.equals(ar.getParentId(), di.getId()))
 							.collect(Collectors.toList())
 				)
 		);
